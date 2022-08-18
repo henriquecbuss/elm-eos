c796b70d6f2664491037c307db623bcf679d5e30
Henrique Buss <henrique.buss@hotmail.com>
2022-08-18 11:51:18 -0300

    Add elm-review rules

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -11,9 +11,60 @@ when inside the directory containing this file.
 
 -}
 
+import Docs.NoMissing
+import Docs.ReviewAtDocs
+import Docs.ReviewLinksAndSections
+import Docs.UpToDateReadmeLinks
+import NoDebug.Log
+import NoDebug.TodoOrToString
+import NoExposingEverything
+import NoImportingEverything
+import NoMissingTypeAnnotation
+import NoMissingTypeAnnotationInLetIn
+import NoMissingTypeExpose
+import NoPrematureLetComputation
+import NoPrimitiveTypeAlias
+import NoRecordAliasConstructor
+import NoSimpleLetBody
+import NoUnused.CustomTypeConstructorArgs
+import NoUnused.CustomTypeConstructors
+import NoUnused.Dependencies
+import NoUnused.Exports
+import NoUnused.Modules
+import NoUnused.Parameters
+import NoUnused.Patterns
+import NoUnused.Variables
 import Review.Rule exposing (Rule)
+import Simplify
 
 
 config : List Rule
 config =
-    []
+    [ Docs.NoMissing.rule
+        { document = Docs.NoMissing.onlyExposed
+        , from = Docs.NoMissing.exposedModules
+        }
+    , Docs.ReviewAtDocs.rule
+    , Docs.ReviewLinksAndSections.rule
+    , Docs.UpToDateReadmeLinks.rule
+    , NoDebug.Log.rule
+    , NoDebug.TodoOrToString.rule
+    , NoExposingEverything.rule
+    , NoImportingEverything.rule []
+    , NoMissingTypeAnnotation.rule
+    , NoMissingTypeAnnotationInLetIn.rule
+    , NoMissingTypeExpose.rule
+    , NoPrematureLetComputation.rule
+    , NoPrimitiveTypeAlias.rule
+    , NoSimpleLetBody.rule
+    , NoUnused.CustomTypeConstructorArgs.rule
+    , NoUnused.CustomTypeConstructors.rule []
+    , NoUnused.Dependencies.rule
+    , NoUnused.Exports.rule
+    , NoUnused.Modules.rule
+    , NoUnused.Parameters.rule
+    , NoUnused.Patterns.rule
+    , NoUnused.Variables.rule
+    , NoRecordAliasConstructor.rule
+    , Simplify.rule Simplify.defaults
+    ]
