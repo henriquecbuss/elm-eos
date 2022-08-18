03dc7fc190edcdc8e29f147b6546460e8f7f0eae
Henrique Buss <henrique.buss@hotmail.com>
2022-08-18 13:14:35 -0300

    Remove default Tests.elm file

--- tests/Tests.elm
+++
@@ -1,8 +0,0 @@
-module Tests exposing (..)
-
-import Test exposing (Test)
-
-
-suite : Test
-suite =
-    Test.todo "Implement the first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
