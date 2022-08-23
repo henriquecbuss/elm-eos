58cd7c5a3f556d104e0d878922dcaada3a1a6eaf
Henrique Buss <henrique.buss@hotmail.com>
2022-08-23 10:17:40 -0300

    Add eosTypeToAnnotation skeleton

--- codegen/src/Generate.elm
+++ codegen/src/Generate.elm
@@ -3,11 +3,88 @@ module Generate exposing (main)
 {-| -}
 
 import Abi
+import Elm
+import Elm.Annotation
+import EosType exposing (EosType)
 import Gen.CodeGen.Generate as Generate
 import Json.Decode
+
+
+
+-- TODO - Convert names to camelCase
 
 
 main : Program Json.Decode.Value () ()
 main =
     Generate.fromJson Abi.decoder
-        (\_ -> [])
+        (\abi ->
+            [ Elm.file [ "Action" ] [ actionsType abi.actions ]
+            ]
+        )
+
+
+actionsType : List Abi.Action -> Elm.Declaration
+actionsType actions =
+    Elm.customType "Action" (List.map actionVariant actions)
+
+
+actionVariant : Abi.Action -> Elm.Variant
+actionVariant action =
+    Elm.variantWith action.name
+        [ Elm.Annotation.record
+            (action.arguments
+                |> List.map (\{ name, type_ } -> ( name, eosTypeToAnnotation type_ ))
+            )
+        ]
+
+
+eosTypeToAnnotation : EosType -> Elm.Annotation.Annotation
+eosTypeToAnnotation eosType =
+    case eosType of
+        EosType.EosBool ->
+            Elm.Annotation.bool
+
+        EosType.EosInt ->
+            Elm.Annotation.int
+
+        EosType.EosFloat ->
+            Elm.Annotation.float
+
+        EosType.TimePoint ->
+            Debug.todo ""
+
+        EosType.TimePointSec ->
+            Debug.todo ""
+
+        EosType.BlockTimestampType ->
+            Debug.todo ""
+
+        EosType.Name ->
+            Elm.Annotation.named [ "Eos", "Name" ] "Name"
+
+        EosType.Bytes ->
+            Debug.todo ""
+
+        EosType.EosString ->
+            Elm.Annotation.string
+
+        EosType.Checksum ->
+            Debug.todo ""
+
+        EosType.PublicKey ->
+            Debug.todo ""
+
+        EosType.Signature ->
+            Debug.todo ""
+
+        EosType.Symbol ->
+            Debug.todo ""
+
+        EosType.SymbolCode ->
+            Debug.todo ""
+
+        EosType.Asset ->
+            Debug.todo ""
+
+        EosType.ExtendedAsset ->
+            Debug.todo ""
