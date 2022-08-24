93189205a05b9d1934b5ab4597fdac435f474310
Henrique Buss <henrique.buss@hotmail.com>
2022-08-24 12:17:47 -0300

    Generate Action.encoder

--- codegen/src/Generate.elm
+++ codegen/src/Generate.elm
@@ -5,6 +5,7 @@ module Generate exposing (main)
 import Abi
 import Elm
 import Elm.Annotation
+import Elm.Case
 import EosType exposing (EosType)
 import Gen.CodeGen.Generate as Generate
 import Gen.Eos.Asset
@@ -17,6 +18,7 @@ import Gen.Eos.Symbol
 import Gen.Eos.SymbolCode
 import Gen.Eos.TimePoint
 import Gen.Eos.TimePointSec
+import Gen.Json.Encode
 import Gen.Time
 import Json.Decode
 
@@ -29,7 +31,12 @@ main : Program Json.Decode.Value () ()
 main =
     Generate.fromJson Abi.decoder
         (\abi ->
-            [ Elm.file [ "Action" ] [ actionsType abi.actions ]
+            [ Elm.file [ "Action" ]
+                [ actionsType abi.actions
+                    |> Elm.expose
+                , encodeAction abi.actions
+                    |> Elm.expose
+                ]
             ]
         )
 
@@ -41,12 +48,107 @@ actionsType actions =
 
 actionVariant : Abi.Action -> Elm.Variant
 actionVariant action =
-    Elm.variantWith action.name
-        [ Elm.Annotation.record
-            (action.arguments
-                |> List.map (\{ name, type_ } -> ( name, eosTypeToAnnotation type_ ))
+    Elm.variantWith action.name [ actionParameter action ]
+
+
+actionParameter : Abi.Action -> Elm.Annotation.Annotation
+actionParameter action =
+    Elm.Annotation.record
+        (action.arguments
+            |> List.map (\{ name, type_ } -> ( name, eosTypeToAnnotation type_ ))
+        )
+
+
+encodeAction : List Abi.Action -> Elm.Declaration
+encodeAction actions =
+    Elm.declaration "encode"
+        (Elm.fn
+            ( "action"
+            , Just
+                (Elm.Annotation.function
+                    [ Elm.Annotation.named [] "Action" ]
+                    Gen.Json.Encode.annotation_.value
+                )
             )
-        ]
+            (\actionToEncode ->
+                Elm.Case.custom actionToEncode
+                    (Elm.Annotation.named [] "Action")
+                    (List.map encodeActionBranch actions)
+            )
+            -- For some reason, if we don't use `withType` here, elm-codegen
+            -- struggles to infer the type of the function and generates an error.
+            |> Elm.withType
+                (Elm.Annotation.function
+                    [ Elm.Annotation.named [] "Action" ]
+                    Gen.Json.Encode.annotation_.value
+                )
+        )
+
+
+encodeActionBranch : Abi.Action -> Elm.Case.Branch
+encodeActionBranch action =
+    Elm.Case.branch1 action.name
+        ( "args", actionParameter action )
+        (\argsRecord ->
+            Gen.Json.Encode.object
+                (List.map
+                    (\argument ->
+                        Elm.tuple (Elm.string argument.name)
+                            (encodeEosType argument.type_ (Elm.get argument.name argsRecord))
+                    )
+                    action.arguments
+                )
+        )
+
+
+encodeEosType : EosType -> Elm.Expression -> Elm.Expression
+encodeEosType eosType =
+    case eosType of
+        EosType.EosBool ->
+            Gen.Json.Encode.call_.bool
+
+        EosType.EosInt ->
+            Gen.Json.Encode.call_.int
+
+        EosType.EosFloat ->
+            Gen.Json.Encode.call_.float
+
+        EosType.TimePoint ->
+            Gen.Eos.TimePoint.call_.encode
+
+        EosType.TimePointSec ->
+            Gen.Eos.TimePointSec.call_.encode
+
+        EosType.BlockTimestampType ->
+            Gen.Time.posixToMillis
+                >> Gen.Json.Encode.call_.int
+
+        EosType.Name ->
+            Gen.Eos.Name.call_.encode
+
+        EosType.EosString ->
+            Gen.Json.Encode.call_.string
+
+        EosType.Checksum ->
+            Gen.Eos.Checksum.call_.encode
+
+        EosType.PublicKey ->
+            Gen.Eos.PublicKey.call_.encode
+
+        EosType.Signature ->
+            Gen.Eos.Signature.call_.encode
+
+        EosType.Symbol ->
+            Gen.Eos.Symbol.call_.encode
+
+        EosType.SymbolCode ->
+            Gen.Eos.SymbolCode.call_.encode
+
+        EosType.Asset ->
+            Gen.Eos.Asset.call_.encode
+
+        EosType.ExtendedAsset ->
+            Gen.Eos.ExtendedAsset.call_.encode
 
 
 eosTypeToAnnotation : EosType -> Elm.Annotation.Annotation
