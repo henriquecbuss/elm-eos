4b39153176865b2f6940898c4b91bf5db78877db
Henrique Buss <henrique.buss@hotmail.com>
2022-08-24 23:30:24 -0300

    Adjust Generate.Table.Query

--- codegen/src/Generate/Table/Query.elm
+++ codegen/src/Generate/Table/Query.elm
@@ -1,15 +1,49 @@
-module Generate.Table.Query exposing (type_)
+module Generate.Table.Query exposing (generateQuery)
 
 import Abi
+import Context exposing (Context)
 import Elm
+import Elm.Annotation
+import Gen.Eos.Query
 import String.Extra
 
 
-type_ : List Abi.Table -> Elm.Declaration
-type_ tables =
-    Elm.customType "Table" (List.map tableVariant tables)
-
-
-tableVariant : Abi.Table -> Elm.Variant
-tableVariant table =
-    Elm.variant (String.Extra.camelize table.name)
+generateQuery : Context -> Abi.Table -> Elm.Declaration
+generateQuery context table =
+    Elm.declaration (String.Extra.camelize table.name)
+        (Elm.fn
+            ( "arg"
+            , Just (Elm.Annotation.record [ ( "scope", Elm.Annotation.string ) ])
+            )
+            (\arg ->
+                Gen.Eos.Query.make_.query
+                    (Elm.record
+                        [ ( "scope", Elm.get "scope" arg )
+                        , ( "indexPosition", Elm.nothing )
+                        , ( "lowerBound", Elm.nothing )
+                        , ( "upperBound", Elm.nothing )
+                        , ( "limit", Elm.nothing )
+                        , ( "reverse", Elm.bool False )
+                        , ( "baseUrl", Elm.string context.baseUrl )
+                        , ( "contract", Elm.string context.contract )
+                        , ( "table", Elm.string table.name )
+                        , ( "decoder"
+                          , Elm.value
+                                { importFrom = [ "Table", "Decoder" ]
+                                , name = String.Extra.camelize table.name
+                                , annotation = Nothing
+                                }
+                          )
+                        ]
+                    )
+            )
+            |> Elm.withType
+                (Elm.Annotation.function
+                    [ Elm.Annotation.record [ ( "scope", Elm.Annotation.string ) ]
+                    ]
+                    (Elm.Annotation.namedWith [ "Eos", "Query" ]
+                        "Query"
+                        [ Elm.Annotation.named [ "Table" ] (String.Extra.camelize table.name) ]
+                    )
+                )
+        )
--- codegen/tests/Generate/Table/QueryTests.elm
+++ codegen/tests/Generate/Table/QueryTests.elm
@@ -1,6 +1,7 @@
 module Generate.Table.QueryTests exposing (suite)
 
 import Abi
+import Context exposing (Context)
 import Elm.ToString
 import EosType
 import Expect
@@ -11,22 +12,69 @@ import Test exposing (Test, describe, test)
 suite : Test
 suite =
     describe "Generate.Table.Query"
-        [ type_
+        [ generateQuery
         ]
 
 
-type_ : Test
-type_ =
-    describe "type_"
+generateQuery : Test
+generateQuery =
+    describe "generateQuery"
         [ test "peopleTable" <|
             \_ ->
-                [ peopleTable ]
-                    |> Generate.Table.Query.type_
+                peopleTable
+                    |> Generate.Table.Query.generateQuery context
                     |> Elm.ToString.declaration
-                    |> .body
-                    |> Expect.equal """type Table
-    = People"""
+                    |> Expect.all
+                        [ .signature
+                            >> Expect.equal "people : { scope : String } -> Eos.Query.Query Table.People"
+                        , .body
+                            >> Expect.equal """people : { scope : String } -> Eos.Query.Query Table.People
+people arg =
+    Eos.Query.Query
+        { scope = arg.scope
+        , indexPosition = Nothing
+        , lowerBound = Nothing
+        , upperBound = Nothing
+        , limit = Nothing
+        , reverse = False
+        , baseUrl = "testurl"
+        , contract = "test.contract"
+        , table = "people"
+        , decoder = Table.Decoder.people
+        }"""
+                        ]
+        , test "snakeCaseTable" <|
+            \_ ->
+                snakeCaseTable
+                    |> Generate.Table.Query.generateQuery context
+                    |> Elm.ToString.declaration
+                    |> Expect.all
+                        [ .signature
+                            >> Expect.equal "snakeCase : { scope : String } -> Eos.Query.Query Table.SnakeCase"
+                        , .body
+                            >> Expect.equal """snakeCase : { scope : String } -> Eos.Query.Query Table.SnakeCase
+snakeCase arg =
+    Eos.Query.Query
+        { scope = arg.scope
+        , indexPosition = Nothing
+        , lowerBound = Nothing
+        , upperBound = Nothing
+        , limit = Nothing
+        , reverse = False
+        , baseUrl = "testurl"
+        , contract = "test.contract"
+        , table = "snake_case"
+        , decoder = Table.Decoder.snakeCase
+        }"""
+                        ]
         ]
+
+
+context : Context
+context =
+    { baseUrl = "testurl"
+    , contract = "test.contract"
+    }
 
 
 peopleTable : Abi.Table
@@ -38,6 +86,17 @@ peopleTable =
           }
         , { name = "assets"
           , type_ = EosType.EosList EosType.Asset
+          }
+        ]
+    }
+
+
+snakeCaseTable : Abi.Table
+snakeCaseTable =
+    { name = "snake_case"
+    , columns =
+        [ { name = "snake_case"
+          , type_ = EosType.EosBool
           }
         ]
     }
