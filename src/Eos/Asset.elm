240e4f15fa246c8229c137e6a0e799e025bf40a4
Henrique Buss <henrique.buss@hotmail.com>
2022-08-26 11:19:09 -0300

    Fix asset encoding and decoding

--- src/Eos/Asset.elm
+++ src/Eos/Asset.elm
@@ -15,6 +15,7 @@ module Eos.Asset exposing
 -}
 
 import Eos.Symbol
+import Eos.SymbolCode
 import Json.Decode
 import Json.Encode
 
@@ -31,19 +32,84 @@ type alias Asset =
 -}
 encode : Asset -> Json.Encode.Value
 encode asset =
-    Json.Encode.object
-        [ ( "amount", Json.Encode.float asset.amount )
-        , ( "symbol", Eos.Symbol.encode asset.symbol )
-        ]
+    let
+        precision : Int
+        precision =
+            Eos.Symbol.precision asset.symbol
+
+        padWithDecimals : String -> String
+        padWithDecimals amountString =
+            if precision == 0 then
+                amountString
+
+            else
+                case String.split "." amountString of
+                    [ withoutDecimals ] ->
+                        withoutDecimals ++ "." ++ String.repeat precision "0"
+
+                    [ number, decimals ] ->
+                        number ++ "." ++ decimals ++ String.repeat (precision - String.length decimals) "0"
+
+                    _ ->
+                        amountString
+    in
+    Json.Encode.string
+        (padWithDecimals (String.fromFloat asset.amount)
+            ++ " "
+            ++ Eos.SymbolCode.toString (Eos.Symbol.code asset.symbol)
+        )
 
 
 {-| Decode an [Asset](#Asset) from JSON.
 -}
 decoder : Json.Decode.Decoder Asset
 decoder =
-    Json.Decode.map2
-        (\amount symbol ->
-            { amount = amount, symbol = symbol }
-        )
-        (Json.Decode.field "amount" Json.Decode.float)
-        (Json.Decode.field "symbol" Eos.Symbol.decoder)
+    Json.Decode.string
+        |> Json.Decode.andThen
+            (\decodedString ->
+                case String.split " " decodedString of
+                    [ amount, symbol ] ->
+                        let
+                            floatAmount : Maybe Float
+                            floatAmount =
+                                String.toFloat amount
+
+                            precision : Maybe Int
+                            precision =
+                                case String.split "." amount of
+                                    [ _ ] ->
+                                        Just 0
+
+                                    [ _, decimals ] ->
+                                        Just (String.length decimals)
+
+                                    _ ->
+                                        Nothing
+
+                            symbolValue : Maybe (Result Eos.Symbol.Error Eos.Symbol.Symbol)
+                            symbolValue =
+                                Maybe.map (\validPrecision -> Eos.Symbol.fromPrecisionAndCodeString validPrecision symbol)
+                                    precision
+                        in
+                        case ( symbolValue, floatAmount ) of
+                            ( Just (Ok validSymbol), Just validAmount ) ->
+                                Json.Decode.succeed
+                                    { amount = validAmount
+                                    , symbol = validSymbol
+                                    }
+
+                            ( Just (Ok _), Nothing ) ->
+                                Json.Decode.fail ("Invalid amount: " ++ amount)
+
+                            ( Just (Err err), _ ) ->
+                                Json.Decode.fail (Eos.Symbol.errorToString err)
+
+                            ( Nothing, Just _ ) ->
+                                Json.Decode.fail ("Invalid symbol: " ++ symbol)
+
+                            ( Nothing, Nothing ) ->
+                                Json.Decode.fail ("Invalid precision and symbol:\n" ++ "Amount: " ++ amount ++ "\nSymbol: " ++ symbol)
+
+                    _ ->
+                        Json.Decode.fail "Invalid asset format"
+            )
--- tests/Eos/AssetTests.elm
+++ tests/Eos/AssetTests.elm
@@ -11,6 +11,7 @@ suite : Test
 suite =
     describe "Eos.Asset"
         [ jsonRoundtrip
+        , encode
         ]
 
 
@@ -25,6 +26,67 @@ jsonRoundtrip =
                             |> Eos.Asset.encode
                             |> Json.Decode.decodeValue Eos.Asset.decoder
                             |> Expect.equal (Ok { amount = 10, symbol = symbol })
+
+                    Err _ ->
+                        Expect.fail "Invalid symbol"
+        ]
+
+
+encode : Test
+encode =
+    describe "encode"
+        [ test "works for precision 0" <|
+            \_ ->
+                case Eos.Symbol.fromPrecisionAndCodeString 0 "EOS" of
+                    Ok symbol ->
+                        { amount = 10, symbol = symbol }
+                            |> Eos.Asset.encode
+                            |> Json.Decode.decodeValue Json.Decode.string
+                            |> Expect.equal (Ok "10 EOS")
+
+                    Err _ ->
+                        Expect.fail "Invalid symbol"
+        , test "works for precision 1 without decimals" <|
+            \_ ->
+                case Eos.Symbol.fromPrecisionAndCodeString 1 "EOS" of
+                    Ok symbol ->
+                        { amount = 10, symbol = symbol }
+                            |> Eos.Asset.encode
+                            |> Json.Decode.decodeValue Json.Decode.string
+                            |> Expect.equal (Ok "10.0 EOS")
+
+                    Err _ ->
+                        Expect.fail "Invalid symbol"
+        , test "works for precision 2 without decimals" <|
+            \_ ->
+                case Eos.Symbol.fromPrecisionAndCodeString 2 "EOS" of
+                    Ok symbol ->
+                        { amount = 10, symbol = symbol }
+                            |> Eos.Asset.encode
+                            |> Json.Decode.decodeValue Json.Decode.string
+                            |> Expect.equal (Ok "10.00 EOS")
+
+                    Err _ ->
+                        Expect.fail "Invalid symbol"
+        , test "works for precision 2 with 1 decimal" <|
+            \_ ->
+                case Eos.Symbol.fromPrecisionAndCodeString 2 "EOS" of
+                    Ok symbol ->
+                        { amount = 10.1, symbol = symbol }
+                            |> Eos.Asset.encode
+                            |> Json.Decode.decodeValue Json.Decode.string
+                            |> Expect.equal (Ok "10.10 EOS")
+
+                    Err _ ->
+                        Expect.fail "Invalid symbol"
+        , test "works for precision 2 with 2 decimals" <|
+            \_ ->
+                case Eos.Symbol.fromPrecisionAndCodeString 2 "EOS" of
+                    Ok symbol ->
+                        { amount = 10.12, symbol = symbol }
+                            |> Eos.Asset.encode
+                            |> Json.Decode.decodeValue Json.Decode.string
+                            |> Expect.equal (Ok "10.12 EOS")
 
                     Err _ ->
                         Expect.fail "Invalid symbol"
