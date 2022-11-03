a246682ce2c282433bdf62819ce39e1f398bf361
Henrique Buss <henriquebusss@gmail.com>
2022-11-03 17:21:37 -0300

    Change / to prepare for multiple transactions

--- examples/transfer/src/Pages/Home_.elm
+++ examples/transfer/src/Pages/Home_.elm
@@ -32,8 +32,12 @@ import View exposing (View)
 {-| The model for this page
 -}
 type alias Model =
-    { privateKey : String
-    , from : String
+    { transfer : Transfer
+    }
+
+
+type alias Transfer =
+    { from : String
     , to : String
     , amount : String
     , symbolPrecision : String
@@ -45,8 +49,14 @@ type alias Model =
 {-| Everything this page can do
 -}
 type Msg
-    = ClickedTransfer
-    | ClickedLogout
+    = ClickedLogout
+    | EnteredTransferFrom String
+    | EnteredTransferTo String
+    | EnteredTransferAmount String
+    | EnteredTransferSymbolPrecision String
+    | EnteredTransferSymbolCode String
+    | EnteredTransferMemo String
+    | ClickedTransfer
 
 
 {-| This is how elm-spa knows what to do with our app
@@ -69,16 +79,20 @@ page shared _ =
 
 init : ( Model, Effect Msg )
 init =
-    ( { privateKey = ""
-      , from = "henriquebuss"
-      , to = "henriquebus2"
-      , amount = "2"
-      , symbolPrecision = "0"
-      , symbolCode = "BUSS"
-      , memo = "Hello from elm-eos!"
-      }
+    ( { transfer = initTransfer }
     , Effect.none
     )
+
+
+initTransfer : Transfer
+initTransfer =
+    { from = "henriquebuss"
+    , to = "henriquebus2"
+    , amount = "2"
+    , symbolPrecision = "0"
+    , symbolCode = "BUSS"
+    , memo = "Hello from elm-eos!"
+    }
 
 
 
@@ -95,11 +109,41 @@ update msg model =
                 |> Effect.fromCmd
             )
 
+        EnteredTransferFrom from ->
+            ( mapTransfer (\transfer -> { transfer | from = from }) model
+            , Effect.none
+            )
+
+        EnteredTransferTo to ->
+            ( mapTransfer (\transfer -> { transfer | to = to }) model
+            , Effect.none
+            )
+
+        EnteredTransferAmount amount ->
+            ( mapTransfer (\transfer -> { transfer | amount = amount }) model
+            , Effect.none
+            )
+
+        EnteredTransferSymbolPrecision symbolPrecision ->
+            ( mapTransfer (\transfer -> { transfer | symbolPrecision = symbolPrecision }) model
+            , Effect.none
+            )
+
+        EnteredTransferSymbolCode symbolCode ->
+            ( mapTransfer (\transfer -> { transfer | symbolCode = symbolCode }) model
+            , Effect.none
+            )
+
+        EnteredTransferMemo memo ->
+            ( mapTransfer (\transfer -> { transfer | memo = memo }) model
+            , Effect.none
+            )
+
         ClickedTransfer ->
             case
                 Maybe.map2 Tuple.pair
-                    (parseModel model)
-                    (Eos.Name.fromString model.from |> Result.toMaybe)
+                    (parseTransfer model.transfer)
+                    (Result.toMaybe (Eos.Name.fromString model.transfer.from))
             of
                 Just ( transfer, actor ) ->
                     ( model
@@ -121,23 +165,28 @@ update msg model =
                     ( model, Effect.none )
 
 
-parseModel : Model -> Maybe Cambiatus.Tk.Action.Action
-parseModel model =
+mapTransfer : (Transfer -> Transfer) -> Model -> Model
+mapTransfer fn model =
+    { model | transfer = fn model.transfer }
+
+
+parseTransfer : Transfer -> Maybe Cambiatus.Tk.Action.Action
+parseTransfer transfer =
     let
         parseQuantity : Maybe Eos.Asset.Asset
         parseQuantity =
             Maybe.map2 Eos.Asset.Asset
-                (String.toFloat model.amount)
+                (String.toFloat transfer.amount)
                 parseSymbol
 
         parseSymbol : Maybe Eos.Symbol.Symbol
         parseSymbol =
             Maybe.andThen
                 (\precision ->
-                    Eos.Symbol.fromPrecisionAndCodeString precision model.symbolCode
+                    Eos.Symbol.fromPrecisionAndCodeString precision transfer.symbolCode
                         |> Result.toMaybe
                 )
-                (String.toInt model.symbolPrecision)
+                (String.toInt transfer.symbolPrecision)
     in
     Maybe.map3
         (\from to quantity ->
@@ -145,11 +194,11 @@ parseModel model =
                 { from = from
                 , to = to
                 , quantity = quantity
-                , memo = model.memo
+                , memo = transfer.memo
                 }
         )
-        (Eos.Name.fromString model.from |> Result.toMaybe)
-        (Eos.Name.fromString model.to |> Result.toMaybe)
+        (Eos.Name.fromString transfer.from |> Result.toMaybe)
+        (Eos.Name.fromString transfer.to |> Result.toMaybe)
         parseQuantity
 
 
@@ -158,7 +207,7 @@ parseModel model =
 
 
 view : Shared.Model -> Model -> View Msg
-view _ _ =
+view _ model =
     let
         inputClass : Html.Attribute msg
         inputClass =
@@ -175,44 +224,96 @@ view _ _ =
                     [ Html.text "Logout" ]
                 ]
             ]
-        , Html.main_ [ class "h-full bg-slate-100" ]
-            [ Html.form
-                [ Html.Events.onSubmit ClickedTransfer
-                , class "flex flex-col"
-                ]
-                [ Html.label []
-                    [ Html.text "Private Key"
-                    , Html.input [ inputClass ] []
+        , Html.main_ [ class "h-full bg-slate-100 pt-4" ]
+            [ Html.div [ class "container mx-auto px-4" ]
+                [ Html.h1 [ class "text-4xl font-bold" ] [ Html.text "elm-eos" ]
+                , Html.div [ class "grid grid-cols-1 md:grid-cols-2 w-full" ]
+                    [ Html.form
+                        [ Html.Events.onSubmit ClickedTransfer
+                        , class "mt-8 w-full p-4 bg-white rounded-lg shadow flex flex-col"
+                        ]
+                        [ Html.h2 [ class "font-bold text-xl" ] [ Html.text "Transfer" ]
+                        , Html.div [ class "flex flex-col gap-2 mt-4" ]
+                            [ viewInput
+                                { label = "From"
+                                , value = model.transfer.from
+                                , onInput = EnteredTransferFrom
+                                , type_ = Text
+                                }
+                            , viewInput
+                                { label = "To"
+                                , value = model.transfer.to
+                                , onInput = EnteredTransferTo
+                                , type_ = Text
+                                }
+                            , viewInput
+                                { label = "Amount"
+                                , value = model.transfer.amount
+                                , onInput = EnteredTransferAmount
+                                , type_ = Number
+                                }
+                            , viewInput
+                                { label = "Symbol Precision"
+                                , value = model.transfer.symbolPrecision
+                                , onInput = EnteredTransferSymbolPrecision
+                                , type_ = Number
+                                }
+                            , viewInput
+                                { label = "Symbol Code"
+                                , value = model.transfer.symbolCode
+                                , onInput = EnteredTransferSymbolCode
+                                , type_ = Text
+                                }
+                            , viewInput
+                                { label = "Memo"
+                                , value = model.transfer.memo
+                                , onInput = EnteredTransferMemo
+                                , type_ = Text
+                                }
+                            ]
+                        , Html.button [ class "w-full rounded mt-6 bg-orange-400 text-white py-2 px-4 hover:bg-orange-300 active:bg-orange-500" ] [ Html.text "Transfer" ]
+                        ]
                     ]
-                , Html.label []
-                    [ Html.text "From"
-                    , Html.input [ inputClass ] []
-                    ]
-                , Html.label []
-                    [ Html.text "To"
-                    , Html.input [ inputClass ] []
-                    ]
-                , Html.label []
-                    [ Html.text "Amount"
-                    , Html.input [ inputClass ] []
-                    ]
-                , Html.label []
-                    [ Html.text "Symbol Precision"
-                    , Html.input [ inputClass ] []
-                    ]
-                , Html.label []
-                    [ Html.text "Symbol Code"
-                    , Html.input [ inputClass ] []
-                    ]
-                , Html.label []
-                    [ Html.text "Memo"
-                    , Html.input [ inputClass ] []
-                    ]
-                , Html.button [ class "bg-orange-400 text-white rounded-full w-40 py-2" ] [ Html.text "Transfer" ]
                 ]
             ]
         ]
     }
+
+
+type InputType
+    = Text
+    | Number
+
+
+viewInput :
+    { label : String
+    , value : String
+    , onInput : String -> msg
+    , type_ : InputType
+    }
+    -> Html.Html msg
+viewInput { label, value, onInput, type_ } =
+    Html.label [ class "border rounded flex relative" ]
+        [ Html.span [ class "absolute w-40 px-4 bg-slate-700 text-white -left-px -inset-y-px text-center flex items-center justify-center rounded-l" ]
+            [ Html.text label ]
+        , Html.input
+            [ class "w-full rounded py-2 pl-44 pr-2"
+            , inputTypeToAttribute type_
+            , Html.Attributes.value value
+            , Html.Events.onInput onInput
+            ]
+            []
+        ]
+
+
+inputTypeToAttribute : InputType -> Html.Attribute msg
+inputTypeToAttribute inputType =
+    case inputType of
+        Text ->
+            Html.Attributes.type_ "text"
+
+        Number ->
+            Html.Attributes.type_ "number"
 
 
 
