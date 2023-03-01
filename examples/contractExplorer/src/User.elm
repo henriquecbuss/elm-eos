05c63d228ca4c9f834890dc63ab671106080372c
Henrique Buss <henriquebusss@gmail.com>
2023-02-28 21:06:06 -0300

    Add docs to User

--- examples/contractExplorer/src/User.elm
+++ examples/contractExplorer/src/User.elm
@@ -1,9 +1,31 @@
-module User exposing (State(..), User, init, isConnected, provider)
+module User exposing
+    ( State(..), User
+    , init
+    , isConnected, provider
+    )
+
+{-| This module helps us represent the state of the user's wallet connection.
+
+@docs State, User
+
+
+## Creating a user
+
+@docs init
+
+
+## Extracting data
+
+@docs isConnected, provider
+
+-}
 
 import Eos.Name
 import WalletProvider exposing (WalletProvider)
 
 
+{-| The state of the user's wallet connection
+-}
 type State
     = NotConnected
     | Connecting WalletProvider
@@ -11,6 +33,8 @@ type State
     | Connected User
 
 
+{-| A user
+-}
 type User
     = User
         { provider : WalletProvider
@@ -18,11 +42,15 @@ type User
         }
 
 
+{-| Initialize a user
+-}
 init : { accountName : Eos.Name.Name, provider : WalletProvider } -> User
 init data =
     User data
 
 
+{-| Check if a user is connected
+-}
 isConnected : State -> Bool
 isConnected state =
     case state of
@@ -33,6 +61,8 @@ isConnected state =
             False
 
 
+{-| Get the provider of a user
+-}
 provider : User -> WalletProvider
 provider (User user) =
     user.provider
