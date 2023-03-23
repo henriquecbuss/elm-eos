82a1f3e83d48f65e676269008ce97168c78f0de4
Henrique Buss <henriquebusss@gmail.com>
2023-03-23 14:46:31 -0300

    Add back codegen/src/index.ts

--- codegen/src/index.ts
+++ codegen/src/index.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import { Elm } from "./Command/GenerateApiFiles.elm";
-import type { ElmApp } from "./Command/GenerateApiFiles.elm";
 import { version } from "../../package.json";
 import fs from "fs/promises";
 import path from "path";
@@ -9,45 +8,44 @@ import path from "path";
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 global.XMLHttpRequest = require("xhr2");
 
-// TODO
-// const app = Elm.Command.GenerateDapp.init({
-//   node: null,
-//   flags: {
-//     argv: process.argv,
-//     versionMessage: version,
-//   },
-// }) as ElmApp;
+const app = Elm.Command.GenerateApiFiles.init({
+  node: null,
+  flags: {
+    argv: process.argv,
+    versionMessage: version,
+  },
+});
 
-// app.ports.interopFromElm.subscribe((fromElm) => {
-//   switch (fromElm.tag) {
-//     case "printAndExitFailure": {
-//       console.log(fromElm.data.message);
-//       break;
-//     }
-//     case "printAndExitSuccess": {
-//       console.log(fromElm.data.message);
-//       break;
-//     }
-//     case "writeToFiles": {
-//       Promise.all(
-//         fromElm.data.files.map(async (file) => {
-//           const actualPath = path.join(fromElm.data.output, file.path);
-//           const dirname = path.dirname(actualPath);
+app.ports.interopFromElm.subscribe((fromElm) => {
+  switch (fromElm.tag) {
+    case "printAndExitFailure": {
+      console.log(fromElm.data.message);
+      break;
+    }
+    case "printAndExitSuccess": {
+      console.log(fromElm.data.message);
+      break;
+    }
+    case "writeToFiles": {
+      Promise.all(
+        fromElm.data.files.map(async (file) => {
+          const actualPath = path.join(fromElm.data.output, file.path);
+          const dirname = path.dirname(actualPath);
 
-//           await fs.mkdir(dirname, { recursive: true });
+          await fs.mkdir(dirname, { recursive: true });
 
-//           return fs.writeFile(actualPath, file.contents, { flag: "w+" });
-//         })
-//       )
-//         .then(() => {
-//           app.ports.interopToElm.send({ tag: "finishedWritingToFiles" });
-//         })
-//         .catch((err) => {
-//           console.log(err);
-//           app.ports.interopToElm.send({
-//             tag: "finishedWritingToFilesWithError",
-//           });
-//         });
-//     }
-//   }
-// });
+          return fs.writeFile(actualPath, file.contents, { flag: "w+" });
+        })
+      )
+        .then(() => {
+          app.ports.interopToElm.send({ tag: "finishedWritingToFiles" });
+        })
+        .catch((err) => {
+          console.log(err);
+          app.ports.interopToElm.send({
+            tag: "finishedWritingToFilesWithError",
+          });
+        });
+    }
+  }
+});
